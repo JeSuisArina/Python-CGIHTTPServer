b7c424219691539c0a4f8994daa701e6be702a54
Arina <leonovaap@gmail.com>
2018-05-13 20:25:20 +0300

    allin

---
+++ cgi-bin/cgi.py
@@ -0,0 +1,7 @@
+#!C:/Users/Арина/AppData/Local/Programs/Python/Python37/python
+
+a = [1, 2, 78]
+sm = sum(a)
+print("Content-type:text/html\n\n")
+print('<title>Сумма</title>')
+print(a[0], "+", a[1], "+", a[2], "=", sm)
---
+++ server.py
@@ -0,0 +1,27 @@
+#!C:/Users/Арина/AppData/Local/Programs/Python/Python37
+
+##подключение необходимых библиотек 
+##веб-сервер ##обработчик запросов
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+##отчеты об ошибках CGI
+import cgitb; 
+cgitb.enable()
+
+##создание сервера и обработчика запросов
+server = HTTPServer
+handler = CGIHTTPRequestHandler
+
+##определение адреса для сервера
+##"" = localhost
+## 8000 = порт
+server_address = ("", 8000)
+
+
+
+##создание сервера с передачей ему адреса сервера, номера порта и обработчика
+httpd = server(server_address, handler)
+
+##помещение программы в бесконечный цикл
+##чтобы сервер работал, пока я его не остановлю (Ctrl+C)
+httpd.serve_forever()
