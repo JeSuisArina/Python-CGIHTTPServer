c9a25d08148b6063bb621360c8ece6995a250194
Arina <leonovaap@gmail.com>
2018-05-18 15:40:05 +0300

    +

--- cgi-bin/cgi.py
+++ cgi-bin/cgi.py
@@ -5,10 +5,13 @@ import cgi
 print("Content-type:text/html\n\n")
 print("<title>Сумма</title>")
 form = cgi.FieldStorage()
-a = form.getlist('num', 0)
+a = form.getlist("num")
+sum = 0
 if a:
     for i in range(len(a)-1):
+        sum = sum + int(a[i])
         print(a[i], "+")
-print(a[-1], "=", sum(a))
 else:
-    print("Введите числа")
+	print("Введите числа")
+sum = sum + int(a[-1])
+print(a[-1], "=", sum)
